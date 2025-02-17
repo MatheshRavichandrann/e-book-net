7084e84dbb7c2b89fd1e4e4cbdbbbd0765096b2f
Mathesh <mathesh1907@gmail.com>
2025-02-18 00:59:53 +0530

    minor updates

--- book-network/src/main/java/com/mugiwara/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/mugiwara/book/BookNetworkApiApplication.java
@@ -8,23 +8,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication(scanBasePackages = "com.mugiwara.book")
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
-//@RequiredArgsConstructor
 public class BookNetworkApiApplication {
 
-//    private final RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BookNetworkApiApplication.class, args);
     }
-
-    /*@Bean
-    public CommandLineRunner runner(RoleRepository roleRepository) {
-        return args -> {
-            if (roleRepository.findByName("USER").isEmpty()) {
-                roleRepository.save(
-                        Role.builder().name("USER").build()
-                );
-            }
-        };
-    }*/
 }
